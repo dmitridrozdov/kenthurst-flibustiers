6839f03c02598944ed4134a344edf0e02b63fb30
dmitridrozdov <dm.drozdov@gmail.com>
2026-06-19 14:27:13 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Ubuntu } from 'next/font/google'
 import './globals.css'
+import ConvexClientProvider from './ConvexClientProvider'
 
 const ubuntu = Ubuntu({
   subsets: ['latin'],
@@ -21,7 +22,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   )
 }
